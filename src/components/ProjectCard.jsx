c93db4f496c37bc74861096e7fbde23122548230
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2025-07-21 15:10:31 -0400

    chore: add flex-wrap to tags

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,6 @@
 export function ProjectCard({ project }) {
   const skills = project.skills.map((skill) => (
-    <div className="rounded-md border-2 border-blue-300 border-opacity-75 px-2 text-sm">
+    <div className="rounded-lg border-2 border-blue-300 border-opacity-75 px-2 text-sm">
       {skill}
     </div>
   ))
@@ -8,13 +8,11 @@ export function ProjectCard({ project }) {
     <>
       <a
         href={project.url}
-        className="delay-50 grid-col grid gap-1 rounded-xl border-2 border-solid border-gray-700 p-8 transition duration-200 ease-in-out hover:-translate-y-1 hover:border-gray-400 hover:bg-slate-800"
+        className="delay-50 grid-col grid gap-2 rounded-xl border-2 border-solid border-gray-700 p-8 transition duration-200 ease-in-out hover:-translate-y-1 hover:border-gray-400 hover:bg-slate-800"
       >
         <div className="text-xl font-semibold">{project.name}</div>
         <div>{project.description}</div>
-        <div className="flex flex-col place-items-center gap-1 sm:flex-row">
-          {skills}
-        </div>
+        <div className="flex flex-row flex-wrap gap-1">{skills}</div>
       </a>
     </>
   )
